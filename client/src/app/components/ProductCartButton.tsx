da35c293c036c3c1ad2feccb89a4c2fe74654ca3
srijan mukherjee <emailofsrijan@gmail.com>
2023-03-10 02:26:21 +0530

    feat/frontend: cart button basic functionality

--- client/src/app/components/ProductCartButton.tsx
+++ client/src/app/components/ProductCartButton.tsx
@@ -1,20 +1,76 @@
-import { Box, Button } from "@mui/material";
+import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
 import { Product } from "../models/product";
 import { LoadingButton } from "@mui/lab";
 import { useAppDispatch, useAppSelector } from "../store";
 import { addBasketItem } from "../../features/basket/basketSlice";
+import { Add, Remove } from "@mui/icons-material";
 
 interface Props {
 	product: Product;
 }
 
+function UpdateCartButton({ product }: Props) {
+	const theme = useTheme();
+	const dispatch = useAppDispatch();
+	const { status, context, basket } = useAppSelector((state) => state.basket);
+	const basketItem = basket?.basketItems.find(
+		(item) => item.product!.id === product.id
+	);
+
+	const update = (change: number) => {
+		return () => {
+			dispatch(
+				addBasketItem({ productId: product.id!, quantity: change })
+			);
+		};
+	};
+
+	return (
+		<>
+			<Box
+				sx={{
+					display: "inline-flex",
+					alignItems: "center",
+					padding: "0.5em",
+					border: "2px solid",
+					borderColor: theme.palette.primary.dark,
+					borderRadius: "99999px",
+				}}>
+				<IconButton
+					onClick={update(-1)}
+					disabled={
+						status === "loading" &&
+						context?.productId === product.id
+					}>
+					<Remove />
+				</IconButton>
+				<Typography sx={{ textAlign: "center", px: "2em" }}>
+					{basketItem?.quantity}
+				</Typography>
+				<IconButton
+					onClick={update(1)}
+					disabled={
+						status === "loading" &&
+						context?.productId === product.id
+					}>
+					<Add />
+				</IconButton>
+			</Box>
+		</>
+	);
+}
+
 export default function ProductCartButton({ product }: Props) {
 	const dispatch = useAppDispatch();
-	const { status, context } = useAppSelector((state) => state.basket);
+	const { status, context, basket } = useAppSelector((state) => state.basket);
 
 	const handleCLick = () => {
 		dispatch(addBasketItem({ productId: product.id!, quantity: 1 }));
 	};
+
+	if (basket?.basketItems.find((item) => item.product?.id === product.id)) {
+		return <UpdateCartButton product={product} />;
+	}
 
 	return (
 		<Box>
